6a6180d769bf90b251fe37fa3531e79fd4e06bb6
Andrey Ozornin <andrey@ozorn.in>
2024-09-17 23:21:20 +0200

    add redirects and add the propaganda post

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,4 +8,14 @@ import sitemap from '@astrojs/sitemap';
 export default defineConfig({
 	site: 'https://ozorn.in',
 	integrations: [mdx(), sitemap()],
+	redirects: {
+		'/why-typeof-null-object/': '/blog/typeof-null',
+		'/utro-stepana-nikolaevicha/': '/blog/stepan-nikolaevich',
+		'/2048/': '/blog/2048',
+		'/norma-2-0/': '/blog/norma-2-0',
+		'/quit-smoking/': '/blog/quit-smoking-1',
+		'/quit-day-3/': '/blog/quit-smoking-2',
+		'/quit-2w/': '/blog/quit-smoking-3',
+		'/propaganda/': '/blog/propaganda',
+	}
 });
