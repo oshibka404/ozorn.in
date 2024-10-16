129b961e53cc2de3e3272115667c3e5f495c3d9f
Andrey Ozornin <andrey@ozorn.in>
2024-10-16 09:53:59 +0200

    separate russian and english posts

--- src/pages/rss-ru.xml.js
+++ src/pages/rss-ru.xml.js
@@ -3,11 +3,11 @@ import { getCollection } from 'astro:content';
 import { SITE_TITLE, SITE_DESCRIPTION } from '../consts';
 
 export async function GET(context) {
-	const posts = (await getCollection('blog')).filter(post => post.data.lang === 'en').sort(
+	const posts = (await getCollection('blog')).filter(post => post.data.lang === 'ru').sort(
 		(a, b) => b.data.pubDate.valueOf() - a.data.pubDate.valueOf(),
 	);
 	return rss({
-		title: SITE_TITLE,
+		title: "Андрей Озорнин",
 		description: SITE_DESCRIPTION,
 		site: context.site,
 		items: posts.map((post) => ({
