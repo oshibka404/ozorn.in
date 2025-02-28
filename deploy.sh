8ee7b913ac332ab0c4381561718dd4f07cf4284a
Andrey Ozornin <andrey@ozorn.in>
2025-02-28 09:02:07 +0100

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,4 @@
-source ./.env.local
-rsync --progress -e "ssh -p $SERVER_SSH_PORT" --recursive ./dist/ $SERVER_LOGIN@$SERVER_IP:$SERVER_PATH
+source ./.env.local;
+yarn install;
+yarn build;
+rsync --progress -e "ssh -p $SERVER_SSH_PORT" --recursive ./dist/ $SERVER_LOGIN@$SERVER_IP:$SERVER_PATH;
